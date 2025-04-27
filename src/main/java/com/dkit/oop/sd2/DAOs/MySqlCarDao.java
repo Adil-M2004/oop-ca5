9aaa278b281656830d3475d0ef4b2c76f70971c7
Adil-M2004 <D00265739@student.dkit.ie>
2025-04-27 15:37:54 +0100

    pt5

--- src/main/java/com/dkit/oop/sd2/DAOs/MySqlCarDao.java
+++ src/main/java/com/dkit/oop/sd2/DAOs/MySqlCarDao.java
@@ -318,53 +318,8 @@ public class MySqlCarDao extends MySqlDao implements CarDaoInterface {
         return carsList;     // may be empty
     }
 
-//    @Override
-//    public List<Car> JsonString() throws DaoException {
-//        Connection connection = null;
-//        PreparedStatement preparedStatement = null;
-//        ResultSet resultSet = null;
-//        List<Car> carsList = new ArrayList<>();
-//
-//        try {
-//            //Get connection object using the getConnection() method inherited
-//            // from the super class (MySqlDao.java)
-//            connection = this.getConnection();
-//
-//            String query = "select * from cars";
-//            preparedStatement = connection.prepareStatement(query);
-//
-//            //Using a PreparedStatement to execute SQL...
-//            resultSet = preparedStatement.executeQuery();
-//            while (resultSet.next()) {
-//                int Id = resultSet.getInt(1);
-//                String make = resultSet.getString(2);
-//                String model = resultSet.getString(3);
-//                int year1 = resultSet.getInt(4);
-//                int price = resultSet.getInt(5);
-//                Car c = new Car(Id, make, model, year1, price);
-//                carsList.add(c);
-//            }
-//        } catch (SQLException e) {
-//            throw new DaoException("findAllCaresultSet() " + e.getMessage());
-//        } finally {
-//            try {
-//                if (resultSet != null) {
-//                    resultSet.close();
-//                }
-//                if (preparedStatement != null) {
-//                    preparedStatement.close();
-//                }
-//                if (connection != null) {
-//                    freeConnection(connection);
-//                }
-//            } catch (SQLException e) {
-//                throw new DaoException("findAllUsers() " + e.getMessage());
-//            }
-//        }
-//        return carsList;     // may be empty
-//    }
-
-
-
-
-}//LB
+    @Override
+    public List<Car> JsonString() throws DaoException {
+        // Call the Jsonstring() method to maintain compatibility with both interfaces
+        return Jsonstring() }
+}
